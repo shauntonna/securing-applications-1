7012986b5b7e6510571138ad8e05cd7151abecbb
Shaun Tonna <Shaun.Tonna.c10256@mcast.edu.mt>
2021-02-23 10:02:21 +0100

    Add project files.

---
+++ securing applications 1/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace securing_applications_1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ securing applications 1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace securing_applications_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
